43790e31b79929af48ec299b5a12011a335b50d3
Fayez Nazzal <soulldev@gmail.com>
2022-07-23 09:00:35 +0300

    feat: :sparkles: setup theme-ui

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,9 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["react", "@typescript-eslint"],
-  rules: {},
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
   settings: {
     react: {
       version: "detect",
