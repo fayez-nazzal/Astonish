0e26baccc395defe96d421750a7d3e40bb0f1217
Fayez Nazzal <soulldev@gmail.com>
2022-12-02 15:56:33 +0200

    Refix Portal component failing tests

--- src/components/Portal/index.tsx
+++ src/components/Portal/index.tsx
@@ -2,5 +2,5 @@ import { createPortal } from "react-dom";
 import { IPortalProps } from "./index.types";
 
 export const Portal = ({ children, node }: IPortalProps) => {
-  return node ? createPortal(children, node) : null;
+  return node ? createPortal(children, node) : <div>{children}</div>;
 };
